e2a9443945414b7c8c02c2c784b8881380a391ac
Денис Смирнов <smirnovdenys88@gmail.com>
2021-01-20 00:03:47 +0200

    added patter adapter

---
+++ src/main/java/com/company/structural/adapter/AdapterJavaToDatabase.java
@@ -0,0 +1,4 @@
+package com.company.structural.adapter;
+
+public class AdapterJavaToDatabase extends JavaApplication implements Database {
+}
---
+++ src/main/java/com/company/structural/adapter/DatabaseRunner.java
@@ -0,0 +1,2 @@
+package com.company.structural.adapter;public class DAtabaseRunner {
+}
